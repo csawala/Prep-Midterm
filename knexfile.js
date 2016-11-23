d4600c2669f9998850fe2e4afff563ed8955a68f
csawala <chris.sawala@gmail.com>
2016-11-23 01:36:26 +0000

    updated to work w/ KNEX

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+        user: "ggtynrilwnquyk",
+        password: '5KKFEdikCuzlm7PcEwYE0JHnIc',
+        database: "d6gpgcsr54b700",
+        host: "ec2-54-83-5-43.compute-1.amazonaws.com",
+        port: 5432,
+        ssl: true
+    },
+  }
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
---
+++ test_knex.js
@@ -0,0 +1,29 @@
+const knexConfig = require("./knexfile")
+const db = require("knex")(knexConfig.development);
+
+
+let input = process.argv[2]
+
+
+function printPeople (result){
+  output = result[0]
+
+  console.log("Searching ...")
+  console.log(`Found: ${result.length} person(s) by the name of '${input}':`)
+  console.log(`${output.id}: ${output.first_name} ${output.last_name}, born ${output.birthdate
+    .toISOString().slice(0,10)}`);
+
+  db.destroy()
+}
+
+
+db.select('id', 'first_name', 'last_name', 'birthdate').from('famous_people')
+  .where('famous_people.first_name', input).orWhere('famous_people.last_name', input)
+.then((result) => {
+  printPeople(result)
+})
+.catch((err) => {
+  console.log(err)
+  console.log("Uh oh! Spaghetti-Oh!")
+  db.destroy()
+})
