5ecaeff9e51e29b40df8dd60ffd06d91d834fa3b
csawala <chris.sawala@gmail.com>
2016-12-01 16:25:47 +0000

    add foreign key from famous_people to milestones

---
+++ migrations/20161201140320_add_famous_people_id.js
@@ -0,0 +1,10 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('milestones', function(table) {
+    table.integer('famous_person_id')
+  })
+};
+
+exports.down = function(knex, Promise) {
+
+};
